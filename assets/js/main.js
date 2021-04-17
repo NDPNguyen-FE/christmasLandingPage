f90b042024fd24ca8d64f17d3e297171da138789
NDPNguyen-FE <ndpn.dev@gmail.com>
2021-04-17 13:43:21 +0700

    commit 1

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/**========================= SHOW MENU ============================== */
+const showMenu = (toogleId, navId) => {
+  const toogle = document.getElementById(toogleId);
+  const nav = document.getElementById(navId);
+
+  if (toogle && nav) {
+    toogle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+
+showMenu("nav__toogle", "nav__menu");
+/**========================= REMOVE MENU MOBILE ============================== */
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav__menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+/**====================== SCROLL SECTIONS ACTIVE LINK ======================= */
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+/**====================== CHANGE BACKGROUND HEADER ======================= */
+function scrollHeader() {
+  const nav = document.getElementById("header");
+
+  if (this.scrollY >= 200) {
+    nav.classList.add("scroll-header");
+  } else {
+    nav.classList.remove("scroll-header");
+  }
+}
+window.addEventListener("scroll", scrollHeader);
+/**====================== SHOW SCROLL TOP ======================= */
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrollY >= 560) {
+    scrollTop.classList.add("show-scroll");
+  } else {
+    scrollTop.classList.remove("show-scroll");
+  }
+}
